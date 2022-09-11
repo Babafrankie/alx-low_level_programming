e5181c966f35e8b8b238b49317b7d21e61f87dbf
Babafrankie <fnwandibie@gmail.com>
2022-09-11 14:48:19 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - printing number from 0-9 with commas and space between them
- * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * main - Entry point
+ *
  * REturn: Always 0
  */
 int main(void)
