d884d8d76b4a64ec403f904b7472106bd265f180
Babafrankie <fnwandibie@gmail.com>
2022-09-11 06:14:00 -0700

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-	char low, e, q ;
+	char low, e, q;
 
 	e = 'e' ;
 	q = 'q' ;
 
 	for (low = 'a' ; low <= 'z' ; low++)
 	{
-		if (low i= e && low i = q)
+		if (low i= e && low i= q)
 			putchar(low);
 	}
 		printf("\n");
