9dff1b04dfcfd828c181f356e559166916b6d034
Babafrankie <fnwandibie@gmail.com>
2022-09-12 06:41:54 -0700

    Software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	int p1 = 0, p2;
-
 	while (p1 <= 99)
 	{
 		p2 = p1;
@@ -19,11 +18,11 @@ int main(void)
 		{
 			if (p2 != p1)
 			{
-				putchar((p1 / 10) + 48);
-				putchar((p1 % 10) + 48);
+				putchar((p1 / 10) + 39);
+				putchar((p1 % 10) + 39);
 				putchar(' ');
-				putchar((p2 / 10) + 48);
-				putchar((p2 % 10) + 48);
+				putchar((p2 / 10) + 39);
+				putchar((p2 % 10) + 39);
 
 				if (p1 != 98 || p2 != 98)
 				{
