05abf128b4a7173aadbd897116dac23c6facbe90
Babafrankie <fnwandibie@gmail.com>
2022-09-18 23:37:00 -0700

    My task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an intas parameter and update the value it pointo 98.
+ * reset_to_98 - takes a pointer to an intas parameter
+ * and update the value it pointo 98.
  * @n: int parameter
  *
  * Return: Nothing
