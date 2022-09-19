4ceca5c3e3c150aede8aec85694f4e06ca1ec812
Babafrankie <fnwandibie@gmail.com>
2022-09-19 00:03:07 -0700

    My task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen - function that returns the length of a string
- * @s this is the input string
+ * @s: this is the input string
  *
  * Return: length of the string
  */
@@ -11,7 +11,7 @@ int _strlen(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0';  index++);
-
+	for (index = 0; s[index] != '\0'; index++)
+		;
 	return (index);
 }
