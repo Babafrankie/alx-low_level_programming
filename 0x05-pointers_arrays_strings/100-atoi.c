46f74d1a2843e43a22f9cd74905479336371404f
Babafrankie <fnwandibie@gmail.com>
2022-09-19 02:39:00 -0700

    My task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+i#include "main.h"
+#include <stio.h>
+/**
+ * _atoi - convert string to aninteger.
+ * @s: pointer to a character string.
+ *
+ * Return: void
+ */
+
+imt _atoi(char *s)
+{
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do
+		{
+			num = num * 10 + (*temp - '0');
+			temp++;
+		}
+		while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
+}
+
