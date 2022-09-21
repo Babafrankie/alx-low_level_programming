1feb9486d7a32da8de33b060777309ccea94dda5
Babafrankie <fnwandibie@gmail.com>
2022-09-21 11:21:19 -0700

    My task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,12 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
-	while (dest[index++])
-		dest_len++;
+	int count = 0, count2 = 0;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
