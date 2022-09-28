59b3ba73e7e303e8ef9c1c67970c766113a6f077
Babafrankie <fnwandibie@gmail.com>
2022-09-28 14:48:42 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,33 @@
 #include "main.h"
+#include <stdio.h>
+
 
 /**
- * _is_prime_number - find natural square root
+ * is_prime_number - check if n is a prime number
  * @n: int
- * Return: int
+ * Return: 0 or 1
  */
+
 
 int is_prime_number(int n)
 {
-	return (square(n, 1));
+	return (check_prime(n, 2));
 }
 
 /**
- * square - find square root
- * @n: int to find square root
- * @val: square root
+ * check_prime - check all number < n if they can divide it
+ * @n: int
+ * @resp: int
  * Return: int
  */
 
-int square(int n, int val)
+int check_prime(int n, int resp)
 {
 
-	if (val * val == n)
-		return (val);
-	else if (val * val < n)
-		return  (square(n, val + 1));
+	if (resp >= n && n > 1)
+		return (1);
+	else if (n % resp == 0 || n <= 1)
+		return (0);
 	else
-		return (-1);
-
+		return (check_prime(n, resp + 1));
 }
