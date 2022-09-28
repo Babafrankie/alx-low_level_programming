c0d3afe27a32b31fde82bc3f83969d508a10e57e
Babafrankie <fnwandibie@gmail.com>
2022-09-28 15:31:44 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,11 +22,11 @@ return (n);
  * Return: 0 or 1
  */
 
-int is_palindrome(char *s)
+int last_index(char *s)
 {
-int end = last_index(s);
+int end = is_palindrome(s);
 
-return (is_palindrome(s, 0, end - 1, end % 2));
+return (check(s, 0, end - 1, end % 2));
 }
 
 /**
